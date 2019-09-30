3f8b2a32677a02a0b5fcf28e838ec518bc568f2b
Dominik Landolt <dominik landolt>
2019-09-30 10:09:44 +0200

    added input args for filepath

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,27 @@
 module Main where
 
+import System.IO
+import System.Environment
+
+import Tokenizer
+import Tokens
+
+-- Argument Dispatcher
+dispatch :: [(String, [String] -> IO ())]
+dispatch = [ ("-f", file) 
+            ]
+
+
+file :: [String] -> IO ()
+file p = do
+  let 
+    s = head p
+  contents <- readFile s
+  putStrLn $ show $ tokenize contents
+
 main :: IO ()
 main = do
   putStrLn "hello world"
+  (command:args) <- getArgs
+  let (Just action) = lookup command dispatch
+  action args
